89ad765440b1c7ff98fe6af6aa0599a441a461c1
yannRVT <yann.rivallant@gmail.com>
2020-07-16 10:01:46 +0200

    change project

---
+++ Setup.R
@@ -0,0 +1,17 @@
+if(!require(devtools)){install.packages("devtools")}
+if(!require(usethis)){install.packages("usethis")}
+if(!require(roxygen2)){install.packages("roxygen2")}
+
+mydir <- "/Users/yannrivallant/Documents/Package/PkgTradeOI/"
+mypackage <- "TradeOI"
+path <- file.path(mydir, mypackage)
+unlink(path, recursive=TRUE)
+
+my_description<-list("Title" = "TradeOI R package",
+                     "Version" ="1.0",
+                     "Authors@R"= "person('Yann', 'Rivallant', email = 'yrivallant@intracen.org', role = c('aut', 'cre'))",
+                     "Description" = "Some functions to compile correctly the TradeOI app.",
+                     "License" = "GPL-3"
+)
+
+create_package(path, my_description, open=FALSE)
